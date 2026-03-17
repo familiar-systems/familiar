d238e22d1648b90591005b97afcad61b07019b86
Michael Grinshpon <mike@loreweaver.no>
2026-03-17 12:01:31 +0100

    Turn on k3s hetzner cluster

--- infra/pulumi-cloud/__main__.py
+++ infra/pulumi-cloud/__main__.py
@@ -13,37 +13,34 @@ See config.py for shared constants.
 import pulumi
 
 import cloud as loreweaver_cloud
-
-# import config as loreweaver_config
-# from k3s_cluster import K3sCluster
-# from k8s import create_k8s_resources
+import config as loreweaver_config
+from k3s_cluster import K3sCluster
+from k8s import create_k8s_resources
 
 # ---------------------------------------------------------------------------
 # k3s cluster (Phase 2: preview subdomain)
-# Uncomment after Scaleway SM secrets are populated (Phase B).
 # ---------------------------------------------------------------------------
-# k3s = K3sCluster(
-#     "k3s",
-#     location=loreweaver_config.LOCATION,
-#     server_type=loreweaver_config.SERVER_TYPE,
-#     image=loreweaver_config.IMAGE,
-#     ssh_keys=[loreweaver_cloud.personal_key.name, loreweaver_cloud.deploy_key.name],
-#     firewall_id=loreweaver_cloud.firewall.id.apply(int),
-#     deploy_private_key=loreweaver_config.read_secret("loreweaver-deploy-ssh-key"),
-#     labels=loreweaver_config.LABELS,
-#     extra_tls_sans=[loreweaver_cloud.coolify_floating_ip.ip_address],
-# )
+k3s = K3sCluster(
+    "k3s",
+    location=loreweaver_config.LOCATION,
+    server_type=loreweaver_config.SERVER_TYPE,
+    image=loreweaver_config.IMAGE,
+    ssh_keys=[loreweaver_cloud.personal_key.name, loreweaver_cloud.deploy_key.name],
+    firewall_id=loreweaver_cloud.firewall.id.apply(int),
+    deploy_private_key=loreweaver_config.read_secret("loreweaver-deploy-ssh-key"),
+    labels=loreweaver_config.LABELS,
+    extra_tls_sans=[loreweaver_cloud.coolify_floating_ip.ip_address],
+)
 
 # ---------------------------------------------------------------------------
 # Kubernetes resources on the k3s cluster
-# Uncomment after Scaleway SM secrets are populated (Phase B).
 # ---------------------------------------------------------------------------
-# create_k8s_resources(
-#     kubeconfig=k3s.kubeconfig,
-#     registry_endpoint=loreweaver_cloud.registry.endpoint,
-#     bunny_api_key=loreweaver_config.read_secret("bunny-api-key"),
-#     acme_email=loreweaver_config.config.require("acme-email"),
-# )
+create_k8s_resources(
+    kubeconfig=k3s.kubeconfig,
+    registry_endpoint=loreweaver_cloud.registry.endpoint,
+    bunny_api_key=loreweaver_config.read_secret("bunny-api-key"),
+    acme_email=loreweaver_config.config.require("acme-email"),
+)
 
 # ---------------------------------------------------------------------------
 # Exports
@@ -63,7 +60,7 @@ pulumi.export("coolify_site_webhook_secret_id", loreweaver_cloud.coolify_site_we
 pulumi.export("bunny_api_key_secret_id", loreweaver_cloud.bunny_api_key_secret.id)
 pulumi.export("k3s_kubeconfig_secret_id", loreweaver_cloud.k3s_kubeconfig_secret.id)
 
-# k3s (Phase 2) -- uncomment with the k3s block above
-# pulumi.export("k3s_floating_ip", k3s.floating_ip_address)
-# pulumi.export("k3s_server_ip", k3s.server_ip)
-# pulumi.export("k3s_kubeconfig", k3s.kubeconfig)
+# k3s (Phase 2)
+pulumi.export("k3s_floating_ip", k3s.floating_ip_address)
+pulumi.export("k3s_server_ip", k3s.server_ip)
+pulumi.export("k3s_kubeconfig", k3s.kubeconfig)
