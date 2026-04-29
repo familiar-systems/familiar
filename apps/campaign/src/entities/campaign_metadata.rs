84149de39291950000d2eff8a79be90c81e4c905
Michael Grinshpon <mike@loreweaver.no>
2026-04-30 00:28:02 +0200

    WIP Add campaign metadata

---
+++ apps/campaign/src/entities/campaign_metadata.rs
@@ -0,0 +1,31 @@
+use chrono::{DateTime, Utc};
+use familiar_systems_app_shared::id::CampaignId;
+use sea_orm::entity::prelude::*;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, DeriveValueType)]
+pub struct CampaignIdCol(pub String);
+
+impl From<CampaignId> for CampaignIdCol {
+    fn from(id: CampaignId) -> Self {
+        Self(id.0)
+    }
+}
+
+impl From<CampaignIdCol> for CampaignId {
+    fn from(col: CampaignIdCol) -> Self {
+        Self(col.0)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, DeriveEntityModel)]
+#[sea_orm(table_name = "campaign_metadata")]
+pub struct Model {
+    // The ID is always 1 since we always have a single campaign metadata record
+    #[sea_orm(primary_key, auto_increment = false, extra = "CHECK (id = 1)")]
+    pub id: i32,
+    pub campaign_id: CampaignIdCol,
+    pub name: String,
+    pub description: Option<String>,
+    pub start_date: DateTime<Utc>,
+    pub end_date: DateTime<Utc>,
+}
