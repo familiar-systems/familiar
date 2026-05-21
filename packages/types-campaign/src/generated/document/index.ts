7ed71fe22bd11b62a4699659ad2610d7b0ea2931
Michael Grinshpon <mike@loreweaver.no>
2026-05-21 13:47:35 +0200

    Sort generated types

--- packages/types-campaign/src/generated/document/index.ts
+++ packages/types-campaign/src/generated/document/index.ts
@@ -1,5 +1,5 @@
 // AUTO-GENERATED barrel. Run `mise run generate-types` to refresh.
 // Do not edit by hand.
 export * from "./ThingHandle";
-export * from "./TocEntryKind";
 export * from "./TocEntry";
+export * from "./TocEntryKind";
